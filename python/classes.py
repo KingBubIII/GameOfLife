b203be9169af73e24504a53d23d1d1b77cff2804
Caleb Richardson <richardsoncaleb60@gmail.com>
2023-10-18 23:54:51 -0600

    started displaying grid using turtle drawing

--- python/classes.py
+++ python/classes.py
@@ -1,15 +1,34 @@
+from turtle import Turtle, Screen
+
 # created mainly for GUI information and alive status
 class CELL():
-    def __init__(self, start_x, start_y) -> None:
-        self.alive = False
-        self.start_x = start_x
-        self.start_y = start_y
+    def __init__(self, size, start_x, start_y) -> None:
+        self.alive = True
+        self.size = size
+        self.start_x = self.size*start_x
+        self.start_y = self.size*start_y
+    
+    # passes turtle object 'pen' to redraw cell
+    def draw(self, pen):
+        pen.penup()
+        pen.goto(self.start_x, self.start_y)
+        pen.pendown()
+        if self.alive:
+            pen.fillcolor("green")
+        else:
+            pen.fillcolor("white")
+        pen.begin_fill()
+        pen.goto(self.start_x, self.start_y+self.size)
+        pen.goto(self.start_x+self.size, self.start_y+self.size)
+        pen.goto(self.start_x+self.size, self.start_y)
+        pen.goto(self.start_x, self.start_y)
+
 
 # creating this allows for easy time mass updating all cells
 class SIMULATION():
     def __init__(self) -> None:
         self.cell_size = 25
-        self.grid = self.create_grid(25, 25)
+        self.grid = self.create_grid(2, 2)
     
     # creates simulation map empty of any live cells for user input later
     def create_grid(self, x, y):
@@ -18,9 +37,16 @@ class SIMULATION():
             
             row = []
             for j in range(x):
-                blank_cell = CELL(self.cell_size*j,self.cell_size*i)
+                blank_cell = CELL(self.cell_size, j, i)
                 row.append(blank_cell)
             
             temp_grid.append(row)
 
         return temp_grid
+    
+    # updating grid includes determining alive status and redrawing cell
+    def update_grid(self, pen):
+        for row in self.grid:
+            for cell in row:
+                print(cell.start_x, cell.start_y)
+                cell.draw(pen)
---
+++ python/main.py
@@ -0,0 +1,22 @@
+from turtle import Turtle, Screen
+from random import random
+from classes import *
+
+if __name__ == '__main__':
+    # inits turtle screen object to draw on
+    canvas = Screen()
+    canvas.setworldcoordinates(0, canvas.window_height(), canvas.window_width(), 0)
+    canvas.tracer(0)
+    
+    # creates turtle drawing object and attaches to current screen object
+    pen = Turtle()
+    # setting defaults for turtle drawing object
+    pen.hideturtle()
+    pen.setheading(90)
+    pen.pendown()
+    pen.pencolor('black')
+
+    sim = SIMULATION()
+    sim.update_grid(pen)
+
+    canvas.mainloop()
